35f2d564e8d7472add5eec58a1e20038ef5ad7c5
heedda <heedda.kollim@gmail.com>
2023-06-02 18:51:49 +0900

    getapi

---
+++ getApi/src/test/java/com/example/getapi/GetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.getapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
